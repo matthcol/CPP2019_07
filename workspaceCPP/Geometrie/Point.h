16d89d0fbdb1d3a8adf5b3abb0e18a87221381f2
Matthias COLIN <matthias.colin@gmail.com>
2019-07-17 08:52:38 +0200

    Add Header files

---
+++ workspaceCPP/Geometrie/Point.h
@@ -0,0 +1,40 @@
+/*
+ * Point.h
+ *
+ *  Created on: 16 juil. 2019
+ *      Author: AELION
+ */
+
+#ifndef POINT_H_
+#define POINT_H_
+
+using namespace std;
+#include <string>
+
+class Point {
+public:
+	Point();
+	Point(const string &nom, double x, double y);
+	Point(const Point &other);
+	virtual ~Point();
+
+	Point& operator=(const Point & other);
+
+	const string& getNom() const;
+	void setNom(const string &nom);
+	double getX() const;
+	void setX(double x);
+	double getY() const;
+	void setY(double y);
+
+	string toString() const;
+	double distance(const Point &other) const;
+	void translater(double deltaX, double deltaY);
+
+private:
+	string nom;
+	double x;
+	double y;
+};
+
+#endif /* POINT_H_ */
